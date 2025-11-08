f44f4fe6db917fffa1caa7b00933a5a54199fc65
pgyunknown <pgokulyadav67893@gmail.com>
2025-11-08 13:32:37 +0530

    first commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -281,7 +281,7 @@ const Home = () => {
           </h1>
 
           <p className="font-mon mt-4 uppercase text-xs sm:text-sm md:text-lg lg:text-xl tracking-widest text-gray-400 text-center">
-            Potential
+            Proclaim Your Potential
           </p>
 
           {/* ⏳ Countdown Timer */}
